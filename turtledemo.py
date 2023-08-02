7e878cc7c788d74411345d95c821e8efb056c11e
Satish Kumar <sathishkumargeesala@gmail.com>
2023-08-02 12:04:06 +0530

    Local Commited Changes for local branch.

--- turtledemo.py
+++ turtledemo.py
@@ -1,11 +1,12 @@
 import turtle
-t=turtle.Turtle()
-s=turtle.Screen()
+
+t = turtle.Turtle()
+s = turtle.Screen()
 s.bgcolor("black")
 t.width(2)
 t.speed(15)
-col=('white','pink','cyan')
-for i in range(300):
-    t.pencolor(col[i%3])
-    t.forward(i*4)
+col = ('white', 'pink', 'cyan')
+for i in range(500):
+    t.pencolor(col[i % 3])
+    t.forward(i * 4)
     t.right(121)
